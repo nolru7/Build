b5ee24ff54018004c3fc9e56bf5d710a507e1632
석훈 이 <nolru7@naver.com>
2019-01-08 13:59:26 +0900

    1차 테스트

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Build
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("안녕하세요!");
         }
     }
 }
